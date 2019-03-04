c55d8d94406306bf1277c51aa880e7bbac7e1f33
Martti Malmi <sirius@iki.fi>
2019-03-04 13:50:38 +0200

    .

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
-ssh identifi 'sh deploy.sh'
-cp -r dist/* ../irislib.github.io/
-cp -r ../iris-lib/example ../irislib.github.io/
-cp -r ../iris-lib/docs ../irislib.github.io/
+#ssh identifi 'sh deploy.sh'
+cp -R dist/* ../irislib.github.io/
+cp -R ../iris-lib/example ../irislib.github.io/
+cp -R ../iris-lib/docs ../irislib.github.io/
 cd ../irislib.github.io
 git commit -a -m '.'
 git push origin master
