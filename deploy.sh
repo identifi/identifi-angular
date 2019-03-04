ad7cf5ff1f7c3f2dbf1ad15c4c2b1ceba5060c58
Martti Malmi <sirius@iki.fi>
2019-03-04 13:51:00 +0200

    .

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-#ssh identifi 'sh deploy.sh'
+ssh identifi 'sh deploy.sh'
 cp -R dist/* ../irislib.github.io/
 cp -R ../iris-lib/example ../irislib.github.io/
 cp -R ../iris-lib/docs ../irislib.github.io/
