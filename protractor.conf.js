43be3181251ec5084fc30e9c8e50318ed7bed302
Martti Malmi <sirius@iki.fi>
2019-08-09 15:00:48 +0300

    hello travis?

--- protractor.conf.js
+++ protractor.conf.js
@@ -20,14 +20,7 @@ exports.config = {
     }
   },
 
-  baseUrl: 'file://' + __dirname + '/dist/index.html',
-  onPrepare: function() {
-      // By default, Protractor use data:text/html,<html></html> as resetUrl, but
-      // location.replace from the data: to the file: protocol is not allowed
-      // (we'll get ‘not allowed local resource’ error), so we replace resetUrl with one
-      // with the file: protocol (this particular one will open system's root folder)
-      browser.resetUrl = 'file://';
-  },
+  baseUrl: 'http://localhost:3000',
 
   // Spec patterns are relative to the current working directory when
   // protractor is called.
